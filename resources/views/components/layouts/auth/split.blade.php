91122502672ad1bc85f9a52a5b3afbc4eccf6db1
Sameh Bilal <sameh.bilal@outlook.com>
2026-06-03 02:57:05 +0300

    up

--- resources/views/components/layouts/auth/split.blade.php
+++ resources/views/components/layouts/auth/split.blade.php
@@ -10,7 +10,7 @@
         class="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div
             class="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-e dark:border-neutral-800">
-            <div class="absolute inset-0 bg-neutral-900 custom-bg"></div>
+            {{-- <div class="absolute inset-0 bg-neutral-900 custom-bg"></div> --}}
             <a href="{{ route('home') }}" class="relative z-20 flex items-center gap-2 text-md font-medium" wire:navigate>
                 <span class="flex h-8 w-8 items-center justify-center rounded-md">
                     <x-app-logo-icon class="me-2 h-7 fill-current text-white" />
