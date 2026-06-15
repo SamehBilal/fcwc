2bb4d21d1bf37a153224b23797f5d9e7cec31907
Sameh Bilal <sameh.bilal@outlook.com>
2026-06-15 14:50:54 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,8 @@ Route::any('/', fn() => abort(404))->name('home');
 
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
-    Volt::route('standings', 'games.standings')->name('dashboard');
-    Volt::route('questions', 'games.questions')->name('games.questions');
+    /* Volt::route('standings', 'games.standings')->name('dashboard');
+    Volt::route('questions', 'games.questions')->name('games.questions'); */
 
     Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
     Volt::route('settings/password', 'settings.password')->name('settings.password');
